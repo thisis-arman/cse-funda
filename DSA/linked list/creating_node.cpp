507ee29d1dfcc0978446209f039a62dac45e8376
Arman Hossen <armanhossen1549@gmail.com>
2025-07-04 17:58:24 +0600

    constructor

--- DSA/linked list/creating_node.cpp
+++ DSA/linked list/creating_node.cpp
@@ -4,15 +4,15 @@ class Node{
     public:
     int val;
     Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
 };
 
 int main()
 {
-    Node a,b,c;
-    a.val=10;
-    b.val=20;
-    c.val=30;
-
+    Node a(10),b(20),c(300);
     a.next= &b;
     b.next= &c;
     c.next= NULL;
