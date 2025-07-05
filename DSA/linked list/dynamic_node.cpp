934fe01dfe39c30b896a9de8f977b128be76633c
Arman Hossen <armanhossen1549@gmail.com>
2025-07-05 17:46:29 +0600

    linkedlist prinitng using loop

--- DSA/linked list/dynamic_node.cpp
+++ DSA/linked list/dynamic_node.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int val;
-    Node* next;
-    Node(int val){
-        this->val= val;
-        this->next= NULL;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
@@ -16,18 +18,20 @@ int main()
     // head store the address of node in stack memory;
     //  and new Node is created in heap memory which is dynamic ;
     // head is just a pointer
-    Node* head = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
 
-    head->next=a;
-    a->next=b;
-    cout << head->val<<" "<<head->next->val <<" "<<head->next->next->val;
+    head->next = a;
+    a->next = b;
+    // cout << head->val<<" "<<head->next->val <<" "<<head->next->next->val;
 
-
-
-
-
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
 
     return 0;
 }
