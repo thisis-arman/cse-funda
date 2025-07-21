2209d414d96f9b6d9dafdf0230bd91b4a05bc9a8
Arman Hossen <armanhossen1549@gmail.com>
2025-07-21 10:29:57 +0600

    stack implementation using list

---
+++ DSA/Stack & queue/stact_using_list.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myStack
+{
+public:
+    vector<int> l;
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    void pop()
+    {
+        l.pop_back();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+    int top()
+    {
+        return l.back();
+    }
+};
+int main()
+{
+    myStack st;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() <<" ";
+        st.pop();
+    }
+    return 0;
+}
