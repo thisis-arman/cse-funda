0b7a469b666178478f50a8e3541f3243ee19ba55
Arman Hossen <armanhossen1549@gmail.com>
2025-07-21 19:26:04 +0600

    all queue

--- DSA/Stack & queue/queue_using_singly_list.cpp
+++ DSA/Stack & queue/queue_using_singly_list.cpp
@@ -1,35 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-class ListNode{
-    public:
+class ListNode
+{
+public:
     int val;
-    ListNode* next;
-    ListNode(int val){
-        this->val=val;
-        this->next=next;
+    ListNode *next;
+    ListNode(int val)
+    {
+        this->val = val;
+        this->next = next;
     }
 };
 
-class myQueue{
-    public:
-    ListNode* head=NULL;
-    ListNode* tail = NULL;
-    int sz=0;
-    void push(int val){
-        ListNode* newNode= new ListNode(val);
-        if(head==NULL){
-            head=tail=newNode;
+class myQueue
+{
+public:
+    ListNode *head = NULL;
+    ListNode *tail = NULL;
+    int sz = 0;
+    void push(int val)
+    {
+        sz++;
+        ListNode *newNode = new ListNode(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
             return;
         }
-        tail->next=newNode;
-        tail=newNode;
+        tail->next = newNode;
+        tail = newNode;
     }
-    void pop(){
-        ListNode* deleteNode= head;
-        head= head->next;
-        if(head==NULL){
-            tail=NULL;
+    void pop()
+    {
+        sz--;
+        ListNode *deleteNode = head;
+        head = head->next;
+        if (head == NULL)
+        {
+            tail = NULL;
         }
+    }
+    int front()
+    {
+        return head->val;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return head == NULL;
     }
 };
 
