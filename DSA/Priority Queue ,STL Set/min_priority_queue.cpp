d1bea728b5b165e5290a2200600b2bfd22341f53
Arman Hossen <armanhossen1549@gmail.com>
2025-08-07 10:44:44 +0600

    min priority queue

---
+++ DSA/Priority Queue ,STL Set/min_priority_queue.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(10);
+    pq.push(20);
+    pq.push(30);
+    cout << pq.top() <<endl;
+    pq.pop();
+    cout << pq.top() <<endl;
+    pq.pop();
+    cout << pq.top() <<endl;
+    pq.pop();
+    return 0;
+}
