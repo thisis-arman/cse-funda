537ff949fc14eb52d012a3034dde54b3b4e9563a
Arman Hossen <armanhossen1549@gmail.com>
2025-08-18 11:26:05 +0600

    adjacency matrix undirected graph

---
+++ algo/Graph  Travarsal/adjacency_matrix_undirected_graph.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+    memset(adj_mat, 0, sizeof(adj_mat));
+    for (int i = 0; i < e; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            if (i == j)
+            {
+                adj_mat[i][j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_mat[a][b] = 1;
+        adj_mat[b][a] = 1;
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
