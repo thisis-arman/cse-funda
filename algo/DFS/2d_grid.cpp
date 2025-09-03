26597638ab591370bdfe5d44e9792f97376f4f05
Arman Hossen <armanhossen1549@gmail.com>
2025-09-03 20:53:41 +0600

    is_valid func added

--- algo/DFS/2d_grid.cpp
+++ algo/DFS/2d_grid.cpp
@@ -3,23 +3,36 @@ using namespace std;
 char grid[105][105];
 bool vis[105][105];
 vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+int n, m;
+
+bool is_valid(int r, int c)
+{
+    // 1. if row/ column is less than zero and more than or equals to the num of columns and row
+    if (r < 0 || r >= n || c < 0 || c >= m)
+    {
+        return false;
+    }
+    return true;
+}
 
 void dfs(int si, int sj)
 {
 
-    cout << si <<" "<< sj << endl;
+    cout << si << " " << sj << endl;
     vis[si][sj] = true;
     for (int i = 0; i < 4; i++)
     {
-        int ci, cj;
-        ci = si + d[i].first;
-        cj = sj + d[i].second;
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        if (is_valid(ci,cj)&&!vis[ci][cj])
+        {
+            dfs(ci, cj);
+        }
     }
 };
 
 int main()
 {
-    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
