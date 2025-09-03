296cffa67d34092f1a275d5a85f8d4ede920fa18
Arman Hossen <armanhossen1549@gmail.com>
2025-09-03 20:46:14 +0600

    insertion

--- algo/DFS/2d_grid.cpp
+++ algo/DFS/2d_grid.cpp
@@ -2,28 +2,37 @@
 using namespace std;
 char grid[105][105];
 bool vis[105][105];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-void dfs(int si,int sj){
+void dfs(int si, int sj)
+{
 
+    cout << si <<" "<< sj << endl;
+    vis[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci, cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+    }
 };
 
 int main()
 {
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i <n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <m; j++)
+        for (int j = 0; j < m; j++)
         {
             cin >> grid[i][j];
         }
     }
 
-    int si,sj;
-    cin>> si>> sj;
-    dfs(si,sj);
-    memset(vis,false,sizeof(vis));
-
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfs(si, sj);
 
     return 0;
 }
