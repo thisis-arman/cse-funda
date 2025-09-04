5a81d0a9efc6f7fef129df223b6ce4c6196f68a2
Arman Hossen <armanhossen1549@gmail.com>
2025-09-04 23:29:28 +0600

    watermelon solved

--- problemset/codeforces/800/A_Watermelon.cpp
+++ problemset/codeforces/800/A_Watermelon.cpp
@@ -6,17 +6,11 @@ int main()
     int w;
     cin>> w;
     vector<int> v;
-    if(w%2!=0){
+    if(w%2!=0 || w==2){
         cout <<"NO";
     }
-    for(int i=1;i<=w;i++){
-        if(w%i==0 && (w%i)%2==0){
-            v.push_back(i);
-        }
+    else {
+        cout <<"YES";
     }
-    for(int x:v){
-        cout << x<<" ";
-    }
-  
     return 0;
 }
