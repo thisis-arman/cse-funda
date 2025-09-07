2f76a37d3df26ce4ce21867ac972711835eb049f
Arman Hossen <armanhossen1549@gmail.com>
2025-09-07 14:55:04 +0600

    next round problem solved

--- problemset/codeforces/800/next_round.cpp
+++ problemset/codeforces/800/next_round.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 int main()
 {
-    
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    int count =0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end(), greater<int>());
+    for (int i = 0; i <= k; i++)
+    {
+        if(v[i]==0){
+            count=0;
+        }
+        if (v[i]!=0 && v[k - 1] == v[k])
+        {
+            // cout << v[i] << " ";
+            count++;
+        }
+        else if(!v[i]==0)
+        {
+        // cout << v[i] <<" -  here ";
+            // cout << v[i] << " ";
+            count++;
+        }
+    }
+
+    cout << count;
+
     return 0;
 }
