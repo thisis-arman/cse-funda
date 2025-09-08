7716b9b93ca998542dad2a89ef59b4bf9836adec
Arman Hossen <armanhossen1549@gmail.com>
2025-09-08 09:06:48 +0600

    next round problem solved

--- problemset/codeforces/800/next_round.cpp
+++ problemset/codeforces/800/next_round.cpp
@@ -1,38 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int n, k;
     cin >> n >> k;
     vector<int> v(n);
-    int count =0;
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        cin >> a;
-        v.push_back(a);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
     }
+
     sort(v.begin(), v.end(), greater<int>());
-    for (int i = 0; i <= k; i++)
-    {
-        if(v[i]==0){
-            count=0;
-        }
-        if (v[i]!=0 && v[k - 1] == v[k])
-        {
-            // cout << v[i] << " ";
-            count++;
-        }
-        else if(!v[i]==0)
-        {
-        // cout << v[i] <<" -  here ";
-            // cout << v[i] << " ";
+
+    int count = 0;
+    int cutoff = v[k - 1]; 
+
+    for (int i = 0; i < n; i++) {
+        if (v[i] > 0 && v[i] >= cutoff) {
             count++;
         }
     }
 
-    cout << count;
-
+    cout << count << endl;
     return 0;
 }
