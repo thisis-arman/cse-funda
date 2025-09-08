407796d94d5e2e1cff4ca321ee913f3fd8f2fb82
Arman Hossen <armanhossen1549@gmail.com>
2025-09-08 09:23:20 +0600

    domino pilling

---
+++ problemset/codeforces/800/A_Domino_piling.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int rec = n * m;
+    int domino = rec / 2;
+    cout << domino;
+    return 0;
+}
