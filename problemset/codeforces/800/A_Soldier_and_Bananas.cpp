826282dc24097a9ab32e32cd9a0d8c352e1f7427
Arman Hossen <armanhossen1549@gmail.com>
2025-09-10 09:41:25 +0600

    soldier and bananas

---
+++ problemset/codeforces/800/A_Soldier_and_Bananas.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, n, w;
+    cin >> k >> n >> w;
+    int cost = 0;
+    for (int i = 1; i <= w; i++)
+    {
+        cost += i * k;
+    }
+
+    if (cost <= n)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << cost - n;
+    }
+
+    return 0;
+}
