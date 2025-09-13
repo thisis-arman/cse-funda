3ef8f9c92fbaaf6584081e36196760a6eae88770
Arman Hossen <armanhossen1549@gmail.com>
2025-09-13 18:27:30 +0600

    updated

--- problemset/codeforces/800/translation.cpp
+++ problemset/codeforces/800/translation.cpp
@@ -5,19 +5,17 @@ int main()
 {
     string str, re_str;
     cin >> str >> re_str;
-    bool is_valid = false;
+    bool is_valid = true;
     int l = str.length() - 1;
     if (str.length() == re_str.length())
     {
         for (int i = 0; i < str.length(); i++)
         {
-            if (str[i] == re_str[l - i])
+            if (str[i] != re_str[l - i])
             {
-                is_valid = true;
-            }
-            else
-            {
+                // cout << str[i] << " " << re_str[l - i] << endl;
                 is_valid = false;
+                break;
             }
         }
     }
