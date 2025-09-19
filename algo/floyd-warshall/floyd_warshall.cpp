24cb5d009d1f4e585973a0b296aa5e4c2a413eef
Arman Hossen <armanhossen1549@gmail.com>
2025-09-19 18:44:07 +0600

    printing weighted grap using adj_matrix

---
+++ algo/floyd-warshall/floyd_warshall.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_mat[i][j] = 0;
+            }
+            else
+            {
+                adj_mat[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    while (e--)
+    {
+        int a, b, c;
+
+        cin >> a >> b >> c;
+        // cout << a << b << c << endl;
+        adj_mat[a][b] = c;
+    }
+
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj_mat[i][j] == INT_MAX)
+            {
+                cout << "INF ";
+            }
+            else
+            {
+                cout << adj_mat[i][j] <<" ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
