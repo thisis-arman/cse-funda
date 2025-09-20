da4f9e59ff88968cb2a55e51e90554f96cbea245
Arman Hossen <armanhossen1549@gmail.com>
2025-09-20 13:08:18 +0600

    floyd warshall

--- algo/floyd-warshall/shortest_routes_2.cpp
+++ algo/floyd-warshall/shortest_routes_2.cpp
@@ -1,61 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n, e;
-    cin >> n >> e;
-    int adj_mat[n][n];
+int main() {
+    int n, e, q;
+    cin >> n >> e >> q;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-            {
-                adj_mat[i][j] = 0;
-            }
-            else
-            {
-                adj_mat[i][j] = INT_MAX;
-            }
-        }
-    }
-    while (e--)
-    {
-        int a, b, c;
+    const long long INF = LLONG_MAX;
+    vector<vector<long long>> adj_mat(n + 5, vector<long long>(n + 5, INF));
+
+    for (int i = 1; i <= n; i++) adj_mat[i][i] = 0;
+
+    while (e--) {
+        int a, b;
+        long long c;
         cin >> a >> b >> c;
-        adj_mat[a][b] = c;
-        // adj_mat[b][a]=a; //undirected
+        adj_mat[a][b] = min(adj_mat[a][b], c);
+        adj_mat[b][a] = min(adj_mat[b][a], c); // undirected
     }
 
-    for (int k = 0; k < n; k++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (adj_mat[i][k] != INT_MAX && adj_mat[k][j] != INT_MAX && adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j])
-                {
+    // Floyd–Warshall
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (adj_mat[i][k] != INF && adj_mat[k][j] != INF
+                    && adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j]) {
                     adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
                 }
             }
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (adj_mat[i][j] == INT_MAX)
-            {
-                cout << "INF ";
-            }
-            else
-            {
-                cout << adj_mat[i][j] << " ";
-            }
-        }
-        cout << endl;
+
+    while (q--) {
+        int s, d;
+        cin >> s >> d;
+        if (adj_mat[s][d] == INF) cout << -1 << "\n";
+        else cout << adj_mat[s][d] << "\n";
     }
 
     return 0;
