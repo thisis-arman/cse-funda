1c7a45971444cb4d779b4568f92fed5997a3ba4c
Arman Hossen <78895174+thisis-arman@users.noreply.github.com>
2025-09-30 19:01:29 +0600

    Create snake.cpp

---
+++ problemset/codeforces/800/snake.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 1) {
+            for (int j = 0; j < m; j++) cout << "#";
+        } else {
+            if (i % 4 == 2) {
+                for (int j = 0; j < m - 1; j++) cout << ".";
+                cout << "#";
+            } else {
+                cout << "#";
+                for (int j = 1; j < m; j++) cout << ".";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
