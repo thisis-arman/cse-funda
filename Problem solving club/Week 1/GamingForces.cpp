7eabdeef9586005c56954f6b0b2982a9dfc2e270
Arman Hossen <armanhossen1549@gmail.com>
2025-10-07 21:18:32 +0600

    draw square

---
+++ Problem solving club/Week 1/GamingForces.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int count=0;
+        int m;
+        cin>>m;
+        
+
+
+    }
+
+    return 0;
+}
