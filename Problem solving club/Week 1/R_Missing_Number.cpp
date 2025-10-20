8c462c8c7ecb1535afefa1c1bc5ab99a0337e740
Arman Hossen <armanhossen1549@gmail.com>
2025-10-20 19:25:07 +0600

    missing numbers

---
+++ Problem solving club/Week 1/R_Missing_Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+    long long sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        long long x;
+        cin >> x;
+        sum += x;
+    }
+
+    long long total = n * (n + 1) / 2;
+    cout << total - sum << "\n";
+
+    return 0;
+}
---
+++ Problem solving club/Week 1/S_Increasing_Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    int arr[t];
+    for(int i =0;i<t;i++){
+        int a;
+        cin>>a;
+        arr[i]=a;
+
+    }
+    for(int i =0;i<t;i++){
+
+    }
+    
+    return 0;
+}
