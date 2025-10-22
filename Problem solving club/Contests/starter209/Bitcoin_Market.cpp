a9a797433df7c52022ce2acb32121aa36e531ce8
Arman Hossen <armanhossen1549@gmail.com>
2025-10-22 20:41:08 +0600

    bitcoin market problem solved

---
+++ Problem solving club/Contests/starter209/Bitcoin_Market.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>4){
+        cout << "NO";
+    }else{
+        cout <<"YES";
+    }
+    return 0;
+}
