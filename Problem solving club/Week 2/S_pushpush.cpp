c523bf4254ec7d2bd9bc20d0ff889ebddd095b25
Arman Hossen <armanhossen1549@gmail.com>
2025-10-26 17:08:19 +0600

    push push

--- Problem solving club/Week 2/S_pushpush.cpp
+++ Problem solving club/Week 2/S_pushpush.cpp
@@ -3,8 +3,43 @@ using namespace std;
 
 int main()
 {
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    // for (auto i : v)
+    // {
+    //     cout << i << " ";
+    // }
+
+    if (n % 2 == 0)
+    {
+        for (int i = n - 1; i >= 0; i = i - 2)
+        {
+            cout << v[i] << " ";
+        }
+        for (int i = 0; i <= n - 1; i += 2)
+        {
+            cout << v[i] << " ";
+        }
+    }else{
+        for (int i = n - 1; i >= 0; i = i - 2)
+        {
+            cout << v[i] << " ";
+        }
+        for (int i = 1; i <= n - 1; i += 2)
+        {
+            cout << v[i] << " ";
+        }
+    }
 
     return 0;
 }
