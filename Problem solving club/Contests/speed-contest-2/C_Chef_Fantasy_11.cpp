b4bb3a35c73a66e8d6b8c9d1632b4bf2582a0a64
Arman Hossen <armanhossen1549@gmail.com>
2025-10-27 21:02:34 +0600

    chef fantasy

---
+++ Problem solving club/Contests/speed-contest-2/C_Chef_Fantasy_11.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int total = n * (n - 1);
+        cout << total << endl;
+    }
+
+    return 0;
+}
