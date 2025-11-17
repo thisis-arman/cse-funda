589e928fc7868e6ea85581af544f8892e36953be
Arman Hossen <armanhossen1549@gmail.com>
2025-11-17 19:07:27 +0600

    two pointers technique to solve nums of smaller

--- Problem solving club/Week 4/B_Number_of_Smaller.cpp
+++ Problem solving club/Week 4/B_Number_of_Smaller.cpp
@@ -3,8 +3,37 @@ using namespace std;
 
 int main()
 {
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a[i] = x;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        b[i] = x;
+    }
+
+    int l = 0, r = 0, cnt = 0;
+    while (r < m)
+    {
+        if (l < n && a[l] < b[r])
+        {
+            cnt++, l++;
+        }
+        else
+        {
+            cout << cnt << " ";
+            r++;
+        }
+    }
 
     return 0;
 }
